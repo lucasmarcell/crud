34a57dbde85a3475bf1375c345f4e8126cf1b945
Lucas <lmarcell18@gmail.com>
2019-04-08 09:55:01 -0300

    Add project

---
+++ frontend/src/main/Routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router'
+
+import Home from '../components/home/Home'
+import UserCrud from '../components/user/UserCrud'
+import UserList from '../components/user/UserList'
+
+export default props => 
+    <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/users' component={UserCrud} />
+        <Route path='/usersList' component={UserList}/>
+        <Redirect from='*' to='/' />
+    </Switch>
+    
